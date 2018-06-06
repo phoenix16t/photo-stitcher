1adc996c676d2c6dc53396ee56186bd77578f6a1
Eddie Jimenez <casadebanho@gmail.com>
2018-06-06 00:37:06 -0700

    Added bootstrap and README.md, and updated basic app structure

---
+++ README.md
@@ -0,0 +1,34 @@
+# Photo Stitcher
+
+## Getting Started
+Clone the repo
+```
+git clone https://github.com/phoenix16t/photo-stitcher.git
+```
+
+### Installing
+Open the project directory and install dependencies
+```
+cd photo-stitcher
+npm install
+```
+Once the dust has settled, start up the application in production mode
+```
+npm start
+```
+This will run the build and serve the project on localhost:3000. You can also run the build and start the node server manually
+```
+npm run build
+node ./src/server/index.js
+```
+Finally, you can run the app in development mode
+```
+npm run start-dev
+```
+This will enable the webpack watcher, which allows for live updating
+
+## Versioning
+Version | Update
+--- | --- |
+0.0.2 | Added bootstrap and README.md, and updated basic app structure
+0.0.1 | Initial commit
--- src/client/components/app/index.js
+++ src/client/components/app/index.js
@@ -2,6 +2,45 @@ import React from 'react';
 
 export default class App extends React.Component {
   render() {
-    return (<div></div>);
+    return (
+      <div className="container app drop-shadow">
+        <div className="row">
+          <div className="col center">
+            <h1 className="title">The Photo Stitcher</h1>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col">
+            <div className="drop-zone inset-shadow container">
+              <div className="row image-row">
+                <div className="image col-3 temp-image-1"></div>
+                <div className="image col-3 temp-image-2"></div>
+                <div className="image col-3 temp-image-3"></div>
+                <div className="image col-3 temp-image-4"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row btn-wrapper">
+          <div className="col center">
+            <button type="button" className="btn">Upload image</button>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col">
+            <div className="stitch-view inset-shadow"></div>
+          </div>
+        </div>
+
+        <div className="row btn-wrapper">
+          <div className="col center">
+            <button type="button" className="btn">Download image</button>
+          </div>
+        </div>
+      </div>
+    );
   };
 };
